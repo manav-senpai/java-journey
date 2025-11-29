d94182ac211d9fc79fe2ef6be651a746a73175f9
manav-senpai <rockstarsigma@gmail.com>
2025-11-30 02:41:12 +0530

    last push before sleeping

---
+++ src/arithmeticOperators.java
@@ -0,0 +1,19 @@
+public class arithmeticOperators {
+    public static void main(String[] args){
+        // order of operation [P E M D A S]
+        // PLEASE escuse my dear aunt sally
+        // please excuse my dope ass swag 🤘
+
+        int a = 32;
+        int b = 23;
+        int anda = a++;
+        int egg = a% 2;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(anda);
+        System.out.println(anda * 2);
+        System.out.println(anda / 3);
+        System.out.println(a % 5 );
+
+    }
+}
