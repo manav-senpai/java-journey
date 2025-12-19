dccce0b94d6ff2eb36357c55f94597e14874e1d3
manav-senpai <rockstarsigma@gmail.com>
2025-12-20 02:01:12 +0530

    starting java

--- src/VARIABLES.java
+++ src/VARIABLES.java
@@ -1,7 +1,47 @@
 public class VARIABLES {
-    public static void main (String[] args){
+    public static void main(String[] args){
+        //integer
+//        int age = 21;
+//        int year = 2025;
+//        int quantity = 1;
+//        System.out.println(quantity);
+//        System.out.println("The year is " + year);
 
+        //double
+        double price = 23.5;
+//        double gpa = 8.36;
+//        double temperature = -12.4;
+//
+//        System.out.println("$" + price);
 
-        
+        //char ( character )
+//        char grade = 'A';
+//        char symbol = '!';
+//        char currency = '$';
+//        System.out.println(currency+ " " + price);
+
+        //boolean
+
+//        boolean isStudent = false;
+//        boolean forSale = false;
+//        boolean isOnline = true;
+//
+//        System.out.println(isOnline);
+//        if(isStudent) {
+//            System.out.println("You are a student");
+//        }else{
+//            System.out.println("You are not a student");
+//        }
+
+        //string
+
+        String name = "Mundane Mann";
+        String favFood = "Pizza";
+        String email = "mundanemann@gmail.com";
+        String car = "Mustang";
+
+        System.out.println("Hello " + name);
+        System.out.println("Your favourite food is " + favFood);
+        System.out.println("Your email address is : " + email);
     }
 }
